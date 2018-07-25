29d432b0f31c80817a036d71535aa2cce7386dd9
Patrick Henry <git@patrickhenry.us>
2018-07-25 14:22:47 -0400

    back to htaccess

--- backend/app/public/index.php
+++ backend/app/public/index.php
@@ -38,9 +38,9 @@ require __DIR__.'/../vendor/autoload.php';
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Access-Control-Allow-Origin');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+// header('Access-Control-Allow-Origin: *');
+// header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Access-Control-Allow-Origin, access-control-allow-methods');
+// header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
 /*
 |--------------------------------------------------------------------------
 | Run The Application
