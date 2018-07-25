d40a29d1a04e251d2bbc3d15fe6cf165e05eccd5
Patrick Henry <git@patrickhenry.us>
2018-07-25 13:04:48 -0400

    hate cors

--- backend/app/public/index.php
+++ backend/app/public/index.php
@@ -39,6 +39,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 
 
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods', 'GET, POST, PUT,         
+DELETE, OPTIONS'");
 /*
 |--------------------------------------------------------------------------
 | Run The Application
