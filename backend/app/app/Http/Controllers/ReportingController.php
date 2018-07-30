3049f7952843fe52b7c0dbb3171c4b5db510cad0
Patrick Henry <git@patrickhenry.us>
2018-07-30 16:31:17 -0400

    missedf

--- backend/app/app/Http/Controllers/ReportingController.php
+++ backend/app/app/Http/Controllers/ReportingController.php
@@ -90,7 +90,7 @@ class ReportingController extends Controller
         $lats= DB::table('RideData')->select('Latitude','Longitude')->where('rideID','=',1)->get();
         $num = count($lats);
        
-       unction distance($lat1, $lon1, $lat2, $lon2, $unit) 
+       function distance($lat1, $lon1, $lat2, $lon2, $unit) 
         {
             $theta = $lon1 - $lon2;
             $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
