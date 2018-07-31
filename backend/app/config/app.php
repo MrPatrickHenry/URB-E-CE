19c1608acd2ffa333b0f3388dd2747b398fdb08f
Patrick Henry <git@patrickhenry.us>
2018-07-31 15:53:53 -0400

    missed comma

--- backend/app/config/app.php
+++ backend/app/config/app.php
@@ -196,8 +196,7 @@ return [
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'JWTAuth'   => Tymon\JWTAuthFacades\JWTAuth::class,
-'JWTFactory' => Tymon\JWTAuthFacades\JWTFactory::class
-
+'JWTFactory' => Tymon\JWTAuthFacades\JWTFactory::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
         'Password' => Illuminate\Support\Facades\Password::class,
