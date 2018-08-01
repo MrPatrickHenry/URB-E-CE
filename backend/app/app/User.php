cc9b7566efd610d4d107476289fa3ebd99475484
Patrick Henry <git@patrickhenry.us>
2018-07-31 23:30:38 -0400

    model

--- backend/app/app/User.php
+++ backend/app/app/User.php
@@ -6,9 +6,37 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Cashier\Billable;
 use Laravel\Passport\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
+
+ /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+
+
+
+
+
+    
     use Notifiable;
         use Billable;
     use HasApiTokens, Notifiable;
