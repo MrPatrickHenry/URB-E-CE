1450ac5f31a4a48590a6edc7221dcdbab5acd451
Patrick Henry <git@patrickhenry.us>
2018-08-01 00:46:47 -0400

    try this

--- backend/app/app/Http/Controllers/AuthenticateController.php
+++ backend/app/app/Http/Controllers/AuthenticateController.php
@@ -48,13 +48,13 @@ public function index()
         // if no errors are encountered we can return a JWT
 
         $JWT=response()->json(compact('token'));
-        $JWTtoken = $JWT[0]->token;
-
+        // $JWTtoken = $JWT[0]->token;
+echo $JWT;
 // return redirect()->action(
 //     'AuthenticateController@index', ['token' => $JWT]
 // );        // return response()->json(compact('token'));
 
-return redirect()->action('AuthenticateController@index', ['token' => $JWTtoken]
+return redirect()->action('AuthenticateController@index', ['token' => $JWT->token]
 );
 
 
