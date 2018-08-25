954c80da72a43db7cab4c70694e842e748eba04a
Patrick Henry <git@patrickhenry.us>
2018-08-24 23:41:36 -0400

    auth update

--- backend/app/app/Http/Controllers/AuthenticateController.php
+++ backend/app/app/Http/Controllers/AuthenticateController.php
@@ -15,65 +15,55 @@ use Illuminate\Support\Facades\Auth;
 class AuthenticateController extends Controller
 {
 
-public function __construct()
-   {
+  public function __construct()
+  {
        // Apply the jwt.auth middleware to all methods in this controller
        // except for the authenticate method. We don't want to prevent
        // the user from retrieving their token if they don't already have it
-       $this->middleware('jwt.auth', ['except' => ['authenticate']]);
-   }
+   $this->middleware('jwt.auth', ['except' => ['authenticate']]);
+ }
 
-public function index()
-{
+ public function index()
+ {
     // Retrieve all the users in the database and return them
     // $users = User::all();
-    $users = User::where('id', '=', Auth::id())->get();
+  $users = User::where('id', '=', Auth::id())->get();
 
- $data = [
-        "status"=> "200",
-        "data"=> [ $users[0]->id]
-      ];
+  $data = [
+    "status"=> "200",
+    "data"=> [ "id"=> $users[0]->id]
+  ];
 
 
-return response()->json($data);
+  return response()->json($data);
 }
 
-    public function authenticate(Request $request)
-    {
-        $credentials = $request->only('email', 'password');
+public function authenticate(Request $request)
+{
+  $credentials = $request->only('email', 'password');
 
- $credentialsError = [
-        "status"=> "401",
-        "message"=>"invalid credentials" ];
+  $credentialsError = [
+    "status"=> "401",
+    "message"=>"invalid credentials" ];
 
- $tokenError = [
-        "status"=> "500",
-        "message"=>"invalid credentials" ];
+    $tokenError = [
+      "status"=> "500",
+      "message"=>"invalid credentials" ];
 
-        try {
+      try {
             // verify the credentials and create a token for the user
-           if (! $token = JWTAuth::attempt($credentials)) {
+       if (! $token = JWTAuth::attempt($credentials)) {
 //error handling
 
-return response()->json($credentialsError);
-            }
-        } catch (JWTException $e) {
+        return response()->json($credentialsError);
+      }
+    } catch (JWTException $e) {
             // something went wrong
-return response()->json($tokenError);
-
-        }
-
-        // if no errors are encountered we can return a JWT
-
-//         $JWT=response()->json(compact('token'));
-//         // $JWTtoken = $JWT[0]->token;
-// echo $JWT;
-// // return redirect()->action(
-// //     'AuthenticateController@index', ['token' => $JWT]
-// // );        // return response()->json(compact('token'));
-
-return redirect()->action('AuthenticateController@index', ['token' => $token]
-);
+      return response()->json($tokenError);
 
     }
+    return redirect()->action('AuthenticateController@index', ['token' => $token]
+  );
+
+  }
 }
