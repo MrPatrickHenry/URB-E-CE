8828593b9537331cc6b77445c2b95229d6ff4fd0
Patrick Henry <git@patrickhenry.us>
2018-09-03 12:33:59 -0400

    home controller

--- backend/app/storage/m_app/app/controllers/home.js
+++ backend/app/storage/m_app/app/controllers/home.js
@@ -1,13 +1,26 @@
-(function() {
-    'use strict';
+// old code above
+ (function() {
+  'use strict';
+  
+  angular
+  .module('app')
+  .controller('HomeController', HomeController);
+  
+  HomeController.$injector = ['$scope'];
+  
+  function HomeController($scope,$http){
+  // $scope.msg = "Rides";
+  userid = localStorage.getItem("uidurbe");
+  var url = 'http://urbe.patrickhenry.us/api/v1/featureservice';
+  $http.post(url)
+  .then(function(response){
+        $scope.featureService = response.data;
+        // $scope.metric = response.data.metric;
+        })
+  .catch(function(error){
+         alert(error);
+         $scope.error = error;
+         });
+  
+  }  })();
 
-    angular
-        .module('app')
-        .controller('HomeController', HomeController);
-
-    HomeController.$injector = ['$scope'];
-
-    function HomeController($scope){
-        $scope.msg = "Angular active!";
-    }
-})();
